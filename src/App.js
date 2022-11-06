8087e5ae103f2efd9a9c52f8134d8327e371a7d8
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-11-06 14:02:21 +0545

    UI for Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import History from './components/History';
+import NewTransaction from './components/NewTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpense />
+        <History />
+        <NewTransaction />
+      </div>
+    </>
   );
 }
 
